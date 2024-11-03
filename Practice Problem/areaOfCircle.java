9b8087ec212b405a9edb2599cc83d8e0571c55f5
helloshalini <shalinijaiswal1101@gmail.com>
2024-11-04 00:26:19 +0530

    first change

---
+++ Practice Problem/areaOfCircle.java
@@ -0,0 +1,12 @@
+// package Practice Problem;
+
+import java.util.*;
+
+public class areaOfCircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float radius = sc.nextFloat();
+        float area = 3.14f * radius * radius ;
+        System.out.println(area);
+    }
+}
