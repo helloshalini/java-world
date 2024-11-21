d346b7bb8406de9c3aa68cf4d3b738f0887a7f5e
helloshalini <shalinijaiswal1101@gmail.com>
2024-11-21 19:05:05 +0530

    new commit

---
+++ Pattern Problem/pattern2.java
@@ -0,0 +1,12 @@
+
+
+public class pattern2 {
+    public static void main(String[] args) {
+        for(int i= 1 ; i<=4 ; i++){
+            for(int star = 1 ; star<=4-i+1 ; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
