2dfc5d668c63bb976c1c8ebf8feb3351e989b611
helloshalini <shalinijaiswal1101@gmail.com>
2024-11-26 11:42:15 +0530

    String Declare

---
+++ Basic/stringDec.java
@@ -0,0 +1,10 @@
+public class stringDec {
+    public static void main(String[] args) {
+        char arr[] = {'a' , 'b'};
+        String str = "abcd";
+        String str2 = new String("xyz");
+        System.out.println(str2);
+        System.out.println(str);
+        System.out.println(arr);
+    }
+}
