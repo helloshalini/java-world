182a2d96f72014ec8f99716af3c2ca19c99301e6
helloshalini <shalinijaiswal1101@gmail.com>
2024-11-26 16:47:30 +0530

    charAt method string

---
+++ Basic/charAtString.java
@@ -0,0 +1,12 @@
+public class charAtString {
+    public static void printLetters(String str){
+        for(int i = 0 ; i <str.length() ; i++);
+        // System.out.println(str.charAt(i) +" ");
+    }
+    public static void main(String[] args) {
+        String str = "Hello Guys";
+        System.out.println(str.charAt(3));
+        System.out.println(str);
+
+    }
+}
