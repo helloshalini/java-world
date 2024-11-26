a5707122299fab544aef7098bfe56044a77b5462
helloshalini <shalinijaiswal1101@gmail.com>
2024-11-26 12:17:42 +0530

    I/p and o/p in string

---
+++ Basic/inOroutSring.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class inOroutSring {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name;
+        // name = sc.next();
+        name = sc.nextLine();
+        System.out.println(name);
+
+    }
+}
