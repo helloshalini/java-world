3f9566e4c3184bba9c1fdeee7002ca87ec0de8a6
helloshalini <shalinijaiswal1101@gmail.com>
2024-11-26 12:26:59 +0530

    Concatenate String

---
+++ Basic/stringConcatenate.java
@@ -0,0 +1,8 @@
+public class stringConcatenate {
+    public static void main(String[] args) {
+        String a = "Apple";
+        String b ="Red";
+        String full = b+" "+a;
+        System.out.println(full);
+    }
+}
