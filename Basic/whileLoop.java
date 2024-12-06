cf8c176d1a0a861c2428372b19420dc44abd6f5e
helloshalini <shalinijaiswal1101@gmail.com>
2024-12-07 02:00:39 +0530

    While Loop

--- Basic/whileLoop.java
+++ Basic/whileLoop.java
@@ -1,14 +1,16 @@
-import java.util.*;
+// import java.util.*;
 
 public class whileLoop {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int add = 0;
+    public static void main(String args[]) {
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        int n = 5;
         int i = 1;
+        int a = 0;
         while(i<=n){
-            add+=i;
+            a= a+i;
             i++;
         }
+        System.out.println(a);
     }
 }
