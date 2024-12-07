1a6a6f9e21ca3d44d407dbb4e157e05f39ac1ef1
helloshalini <shalinijaiswal1101@gmail.com>
2024-12-07 12:40:53 +0530

    Method Overloading

---
+++ Basic/Polimorphism.java
@@ -0,0 +1,24 @@
+//Method Overloading
+
+public class Polimorphism {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        System.out.println(cal.Sum(1,3));
+        System.out.println(cal.Sum((float)1.3,(float)13.3));
+        System.out.println(cal.Sum(1,3,9));
+    }
+}
+
+class Calculator { 
+    int Sum(int a , int b){
+        return a+b;
+    }
+
+    float Sum(float a , float b){
+        return a+b;
+    }
+
+    int Sum(int a , int b , int c){
+        return a+b+c;
+    }
+}
