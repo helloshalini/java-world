8c8907aa2ef3b9ba7aa82316d0fc8b896481d6e5
helloshalini <shalinijaiswal1101@gmail.com>
2024-12-09 13:00:33 +0530

    Interfaces Class

---
+++ Basic/Interfaces.java
@@ -0,0 +1,28 @@
+public class Interfaces {
+    public static void main(String[] args) {
+        Queen q = new Queen();
+        q.moves();
+    }
+}
+
+interface ChessPlayer{
+    void moves();
+}
+
+class Queen implements ChessPlayer{
+    public void moves(){
+        System.out.println("up , down , left , right , diagonal(in all 4 direction)");
+    }
+}
+
+class King implements ChessPlayer{
+    public void moves(){
+        System.out.println("up , down , left , right  , digonal(with 1 step))");
+    }
+}
+
+class Rook implements ChessPlayer{
+    public void moves(){
+        System.out.println("up , down , left , right )");
+    }
+}
