05c16c1c0dd38af500895430ee0e7a10b59db905
helloshalini <shalinijaiswal1101@gmail.com>
2024-12-09 14:47:27 +0530

    Static Keyword

---
+++ Basic/StaticKeyword.java
@@ -0,0 +1,27 @@
+public class StaticKeyword {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.schoolName = "ABCD";
+
+        Student s2 = new Student();
+        System.out.println(s2.schoolName);
+
+        Student s3 = new Student();
+        System.out.println(s3.schoolName);
+    }
+}
+
+class Student{
+    String name;
+    int rollNo;
+
+    static String schoolName;
+
+    void setName(String name){
+        this.name = name;
+    }
+    String getName(){
+        return this.name;
+    }
+
+}
