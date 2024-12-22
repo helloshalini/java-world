a91a1ef84074d3dd5ddf9730d591bf4977e501d5
helloshalini <shalinijaiswal1101@gmail.com>
2024-12-22 13:17:11 +0530

    Recursive Search

--- Linked List/RecursiveSearch.java
+++ Linked List/RecursiveSearch.java
@@ -34,5 +34,39 @@ public class RecursiveSearch {
         tail = newNode;
     }
 
+    public int helper(Node head , int key){
+        if(head == null) {
+           return -1;
+        }
+
+        if(head.data == key) {
+            return 0;
+        }
+        
+        int idx = helper(head.next, key);
+        if(idx == -1) {
+            return -1;
+        }
+
+        return idx+1;
+
+    }
+    public int recSearch(int key) {
+        return helper(head , key);
+
+    }
+
+    public static void main(String[] args) {
+        RecursiveSearch l1 = new RecursiveSearch();
+        l1.addFirst(2);
+        l1.addFirst(1);
+        l1.addLast(4);
+        l1.addLast(5);
+
+        System.out.println(l1.recSearch(4));
+        System.out.println(l1.recSearch(10));
+        
+    }
+
 
 }
