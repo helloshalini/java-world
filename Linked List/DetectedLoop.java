805f76fa5701ae371820ef8b25195d0f8c6f9620
helloshalini <shalinijaiswal1101@gmail.com>
2024-12-23 18:46:17 +0530

    Detecting Loop

--- Linked List/DetectedLoop.java
+++ Linked List/DetectedLoop.java
@@ -11,38 +11,6 @@ public class DetectedLoop {
     public static Node head;
     public static Node tail;
 
-    public void addFirst(int data){
-        Node newNode = new Node(data);
-
-        if(head == null){
-            head = tail = newNode;
-            return;
-        }
-
-        newNode.next = head;
-        head = newNode;
-    }
-
-    public void addLast(int data){
-        Node newNode = new Node(data);
-        
-        if(head == null){
-            head = tail = newNode;
-            return;
-        }
-        tail.next = newNode;
-        tail = newNode;
-    }
-
-    public void print() { 
-        Node temp = head;
-        while(temp != null) {
-            System.out.print(temp.data+"->");
-            temp = temp.next;
-        }
-        System.out.println("null");
-    }
-
     public static boolean isCycle() {
         Node slow = head;
         Node fast = head;
