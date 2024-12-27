3fb61361ed7f5cbe58a65899c56008df53b267c4
helloshalini <shalinijaiswal1101@gmail.com>
2024-12-27 18:08:03 +0530

    Implement Stack using Deque

---
+++ Stack/StackDeque.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class StackDeque {
+    static class Stack {
+        Deque<Integer> deque = new LinkedList<>();
+
+        public void push(int data) {
+            deque.addLast(data);
+        }
+
+        public int pop() {
+            return deque.removeFirst();
+        }
+
+        public int peek() {
+            return deque.getLast();
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        System.out.println("Peek = "+s.peek());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+
+    }
+}
