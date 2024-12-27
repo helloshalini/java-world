7b20a5e0c40d2707148273b7b56318dea008e77e
helloshalini <shalinijaiswal1101@gmail.com>
2024-12-27 11:01:21 +0530

    Implemented Queue using two Stack

---
+++ Queue/QueueStacks.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class QueueStacks {
+    static class Queue {
+        static Stack<Integer> s1 = new Stack<>();
+        static Stack<Integer> s2 = new Stack<>();
+
+        public static boolean isEmpty() {
+            return s1.isEmpty();
+        }
+
+        // add
+        public static void add(int data) {
+            while(!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+
+            s1.push(data);
+
+            while(!s2.isEmpty()) {
+                s1.push(s1.pop());
+            }
+        }
+
+        // remove
+        public static int remove() {
+            if(isEmpty()) {
+                System.out.println("Queue is Empty");
+                return -1;
+            }
+
+            return s1.pop();
+        }
+
+        // peek
+        public static int peek() {
+            if(isEmpty()) {
+                System.out.println("Queue is Empty");
+                return -1;
+            }
+
+            return s1.peek();
+        }
+    }
+    public static void main(String[] args) {
+
+        // Below all the lines are correct 
+
+        // Queue q = new Queue();
+        // q.add(1);
+        // q.add(2);
+        // q.add(3);
+
+        // while(!q.isEmpty()) {
+        //     System.out.println(q.peek());
+        //     q.remove();
+        // }
+
+    }
+}
