c02df7975945eb11fc81d6faf34dd2befd30b07e
helloshalini <shalinijaiswal1101@gmail.com>
2025-01-07 00:41:38 +0530

    Build tree Preorder

---
+++ Binary Tree/Preorder.java
@@ -0,0 +1,40 @@
+
+public class Preorder {
+    static class  Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class Binary {
+        static int idx = -1;
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if(nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+
+        }
+    }
+    public static void main(String[] args) {
+
+        // below all is correct.
+
+        // int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        // Binary tree = new Binary();
+        // Node root = tree.buildTree(nodes);
+        // System.out.println(root.data);
+    }
+}
