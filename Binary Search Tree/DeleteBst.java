908a3e6852482850474d8e83246154e2946d4901
helloshalini <shalinijaiswal1101@gmail.com>
2025-01-09 10:43:09 +0530

    Delete BST

--- Binary Search Tree/DeleteBst.java
+++ Binary Search Tree/DeleteBst.java
@@ -1,4 +1,4 @@
-public class CreateBst {
+public class DeleteBst {
     static class  Node {
         int data;
         Node left;
@@ -39,6 +39,43 @@ public class CreateBst {
 
     }
 
+    public static Node delete(Node root, int val) {
+        if(root.data <val) {
+            root.right = delete(root.right, val);
+        }
+        else if(root.data > val) {
+            root.left = delete(root.left, val);
+        }
+        else {
+            // case 1
+            if(root.left == null && root.right == null) {
+                return null;
+            }
+
+            // case 2
+            if(root.left == null){
+                return root.right;
+            }
+            else if(root.right == null) {
+                return root.left;
+            }
+
+            // case 3
+            Node IS = findInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+
+        return root;
+    }
+
+    public static Node findInorderSuccessor(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
     public static void main(String[] args) {
         int values[] = {5, 1, 3, 4, 2, 7};
         Node root = null;
@@ -49,5 +86,10 @@ public class CreateBst {
 
         inorder(root);
         System.out.println();
+
+        root = delete(root, 3);
+        System.out.println();
+
+        inorder(root);
     }
 }
