601029f601033d0387847073d44764707a987457
helloshalini <shalinijaiswal1101@gmail.com>
2025-01-17 10:51:37 +0530

    TreeSet

---
+++ Hashing/Treeset.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Treeset {
+    public static void main(String[] args) {
+        TreeSet<String> ts = new TreeSet<>();
+        ts.add("Goa");
+        ts.add("Kota");
+        ts.add("Noida");
+        ts.add("Delhi");
+
+        System.out.println(ts);
+    }
+}
