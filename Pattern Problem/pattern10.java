6bbb398242f17920e579d6c15e5fe8db3bb3c1a5
helloshalini <shalinijaiswal1101@gmail.com>
2025-04-29 23:14:06 +0530

    Java

--- Pattern Problem/pattern10.java
+++ Pattern Problem/pattern10.java
@@ -13,7 +13,7 @@ public class pattern10 {
             System.out.println();
         }
 
-        for(int i = n ; i>1 ;i--){
+        for(int i = n ; i>=1 ;i--){
             for(int j = 1 ; j<=i ; j++){
                 System.out.print("*");
             }
